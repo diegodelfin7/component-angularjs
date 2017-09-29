1c647e20eb0b50ebf64cb13e612d99f51486bc24
diegodelfin7 <diegodelfinacharte@gmail.com>
2017-09-29 08:17:23 -0500

    fix component

--- app.js
+++ app.js
@@ -5,9 +5,13 @@ angular.module('app').controller('InitCtrl',InitCtrl);
 InitCtrl.$inject = ['$uibModal'];
 
 angular.module('app').component('sendRecipientsComponent', {
-  template: '<div> holaa</div>',
+  template: '<div> holaa {{ $ctrl | json }} </div>',
   bindings: {
-    resolve: '<',
+    //resolve: '<',
+    codprod : '=',
+    items : '=',
+    fechini : '='
+    //item : '='
   },
   controller: function () {
     var $ctrl = this;
@@ -21,16 +25,42 @@ function InitCtrl($modal){
 
 	self.hola ="hola";
 	console.log('log', self);
+	self.peru ="Holaa Peru";
+
+	self.resumeData = {};
+	self.codProd = "323";
+	self.fechIni = "2017 Julio";
 
 	self.openModal = function() {
 		//var modalInstance = $uibModal.open({
 		var modalInstance = $modal.open({
-			component : 'sendRecipientsComponent',
+			//component : 'sendRecipientsComponent', items="$ctrl.message"  fechIni="$ctrl.message"
+			template : '<send-recipients-component codprod="$ctrl.codprod" items="$ctrl.message"  fechini="$ctrl.fechIniData"></send-recipients-component>',
+			controller : function(){
+				console.log('self',self);
+				this.codprod = self.codProd;
+				this.fechIniData = self.fechIni;
+				this.message = self.peru;
+				console.log('this modal', this);
+			},
+			controllerAs : '$ctrl'
+			
+			/*controller : function(){
+				vm.peru = 'hola'; 
+			},
+			controllerAs :'$ctrl'
+			*/
+
+			/*
 			resolve : {
 				items : function () {
 					return ["hola", "amigo"];
+				},
+				item : function() {
+					return "golang";
 				}
 			}
+			*/
 		});
 	}
 }
